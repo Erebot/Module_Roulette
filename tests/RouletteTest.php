d3fde4ef61e700a6767130e6905865ce13f4bac4
François Poirotte <clicky@erebot.net>
2011-08-23 21:27:35 +0200

    Adapt tests to the new layout.

--- tests/RouletteTest.php
+++ tests/RouletteTest.php
@@ -16,12 +16,6 @@
     along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-require_once(
-    dirname(__FILE__) .
-    DIRECTORY_SEPARATOR . 'testenv' .
-    DIRECTORY_SEPARATOR . 'bootstrap.php'
-);
-
 class   RouletteTestHelper
 extends Erebot_Module_Roulette_Game
 {
